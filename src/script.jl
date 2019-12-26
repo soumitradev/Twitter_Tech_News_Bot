e7f1e5ea13e189cf9605139add6cd532eef009a2
soumitradev <soumitra.shewale.dev@gmail.com>
2019-12-26 21:36:36 +0530

    Add News Filter, Duplicate Checker
    Add filter to detect news, and detect already retweeted tweet ids so that they aren't retweeted again.

--- src/script.jl
+++ src/script.jl
@@ -1,6 +1,5 @@
 using OAuth, HTTP, JSON
 
-
 oauth_consumer_key = "3APw4LnxlOgKzPoaa7EFwbOXZ"
 oauth_consumer_secret = "oP9GrVUow2ivMsijrO0yxIhVhA2T4iltjeKPBaFbCbskeL1VN5"
 
@@ -11,21 +10,42 @@ post_url = "https://api.twitter.com/1.1/statuses/update.json"
 retweet_url = "https://api.twitter.com/1.1/statuses/retweet/:id.json"
 get_following = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
+number_of_tweets = 10
+# Number of tweets to get every 15 minutes
+trigger_words = ["now", "new", "latest", "see", "update", "feature", "check", "announce", "announcing", "announced", "today", "unveil", "introducing", "release"]
+
 get_oauth(endpoint, options) = oauth_request_resource(endpoint, "GET", options, oauth_consumer_key, oauth_consumer_secret, oauth_token, oauth_token_secret)
 tweet_oauth(endpoint, options) = oauth_request_resource(endpoint, "POST", options, oauth_consumer_key, oauth_consumer_secret, oauth_token, oauth_token_secret)
 
-
 function tweet()
-    abc = get_oauth(get_following, Dict("count" => "5"))
-    jsonCode = String(abc.body)
+    retweeted_ids = JSON.parse(string(read("tweeted.json", String)))
+
+    tweets = get_oauth(get_following, Dict("count" => "$number_of_tweets"))
+    jsonCode = String(tweets.body)
     data = JSON.parse(jsonCode)
-    for i in 1:5
+    acquired_tweets = []
+    for i in 1:number_of_tweets
         id = data[i]["id_str"]
+        if id in retweeted_ids
+            continue
+        end
+        for trigger_word in trigger_words
+            if occursin(trigger_word, lowercase(data[i]["text"]))
+                push!(acquired_tweets, id)
+            end
+        end
+    end
+
+    open("tweeted.json", "w+") do io
+        write(io, JSON.json(union(retweeted_ids, acquired_tweets)))
+    end;
+
+    for j in 1:length(acquired_tweets)
+        id = acquired_tweets[j]
         options = Dict("id" => "$id")
         retweet_url = "https://api.twitter.com/1.1/statuses/retweet/$id.json"
         mytweet = tweet_oauth(retweet_url, options)
     end
-    # println("Tweet Status: ", mytweet.status)
 end
 
 tweet()
